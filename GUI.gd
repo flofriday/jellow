e03c63661da0fc0388b55b2b534365b62ebc165d
flofriday <flohacksfriday@gmail.com>
2020-02-18 01:17:06 +0100

    Add score

--- GUI.gd
+++ GUI.gd
@@ -1,11 +1,26 @@
-extends Control
-
+extends CanvasLayer
+ 
 signal start_game
+
+var score = 0
+
+func _ready():
+	$MarginContainer/ScoreLabel.visible = false
+	$StartUI/CenterContainer/VBoxContainer/Score.visible = false
 
 func _on_Button_pressed():
 	$StartUI.visible = false
+	set_score(0)
+	$MarginContainer/ScoreLabel.visible = true	
 	emit_signal("start_game")
 
 func show_game_over():
 	$StartUI/CenterContainer/VBoxContainer/Title.text = "Game Over"
+	$StartUI/CenterContainer/VBoxContainer/Score.text = "Score: " + str(score)
+	$StartUI/CenterContainer/VBoxContainer/Score.visible = true
 	$StartUI.visible = true
+	$MarginContainer/ScoreLabel.visible = false
+
+func set_score(_score):
+	score = _score
+	$MarginContainer/ScoreLabel.text = str(score)
--- World.gd
+++ World.gd
@@ -11,6 +11,7 @@ func _physics_process(delta):
 	# Set the camera 
 	if $CameraController.position.y > $Player.position.y:
 		$CameraController.position.y = $Player.position.y
+		$GUI.set_score(calc_score())
 
 	# Set gameover if the player is lower than the camera
 	if $Player.position.y > $CameraController.position.y + get_viewport().size.y/2:
@@ -32,6 +33,11 @@ func game_over():
 	$Player.position = center
 	$Player.freeze = true
 	$Player.visible = false
+
+func calc_score():
+	var center = get_viewport().size/2
+	var pos = $Player.position.y - center.y
+	return int(pos / 100) * -1
 
 # Start the game if the GUI says so
 func _on_GUI_start_game():
